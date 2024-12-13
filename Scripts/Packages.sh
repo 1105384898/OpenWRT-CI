7afba21a5e267a2955110e58e6a605ee57f4cca6
yangyang97 <46973226+1105384898@users.noreply.github.com>
2024-12-13 12:41:39 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,7 +40,7 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 UPDATE_PACKAGE "luci-app-autotimeset" "sirpdboy/luci-app-autotimeset" "master"
-UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
+#UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
