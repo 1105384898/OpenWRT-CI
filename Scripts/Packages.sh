29ae2c2fe622156d17453b10566c84f18651b083
yangyang97 <46973226+1105384898@users.noreply.github.com>
2025-07-23 09:59:26 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -70,8 +70,6 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
-UPDATE_PACKAGE "watchdog" "sirpdboy/luci-app-watchdog" "main"
-
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
